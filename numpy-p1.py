90d632bed4c615d6380ddd8aee70735650db3813
kianaseraj <105077062+kianaseraj@users.noreply.github.com>
2023-03-30 19:01:23 +0200

    Update numpy-p1.py

--- numpy-p1.py
+++ numpy-p1.py
@@ -1,6 +1,5 @@
 #finding π with numpy library which is much faster than using for and if loops.
 import numpy as np
-import matplotlib.pyplot as plt
 N_run = 100
 N = 100_000
 pos = np.random.uniform(-1, 1, size=(N_run, N, 2))
@@ -8,5 +7,4 @@ dist = np.sum(pos**2, axis=2)
 num_inside = np.sum(dist <= 1, axis=1)
 pi_estimate = 4 * num_inside / N
 print(pi_estimate)
-plt.hist(pi_estimate)
-plt.show()
+
