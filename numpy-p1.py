1997cc0f310c73f8adde70eec24ea352bb7bb23c
kianaseraj <105077062+kianaseraj@users.noreply.github.com>
2023-03-30 18:54:08 +0200

    Add files via upload

---
+++ numpy-p1.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+N_run = 100
+N = 100_000
+pos = np.random.uniform(-1, 1, size=(N_run, N, 2))
+dist = np.sum(pos**2, axis=2)
+num_inside = np.sum(dist <= 1, axis=1)
+pi_estimate = 4 * num_inside / N
+print(pi_estimate)
+plt.hist(pi_estimate)
+plt.show()
